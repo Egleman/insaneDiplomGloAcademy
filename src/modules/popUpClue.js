e064cd4b169a0397571fc3e732aaad48220cb337
Sergey Tablet <martsnov@hotmail.com>
2022-01-22 00:30:00 +0300

    Реализация подсказки без почищенных комментариев

--- src/modules/popUpClue.js
+++ src/modules/popUpClue.js
@@ -84,8 +84,38 @@ const clue = () => {
         let item = elem.closest('.formula-item');
         item.style.zIndex = '';
         item.querySelector('.formula-item-popup').classList.remove('formula-item-active');
-        //item.querySelector('.formula-item-popup').classList.remove('formula-item-trans');
-        //item.querySelector('.formula-item__descr').style.opacity = '1';
+        if (item.querySelector('.formula-item-popup').classList.contains('formula-item-trans')) {
+            item.querySelector('.formula-item-popup').classList.remove('formula-item-trans');
+        }
+        
+        if (item.querySelector('.formula-item-popup-01')) {
+            //console.log('есть');
+            item.querySelector('.formula-item-popup-01').style.transform = 'rotate(0deg) translateY(-5px)';
+            item.querySelector('.formula-item__descr').style.opacity = '1';
+        } else if (item.querySelector('.formula-item-popup-02')) {
+            //console.log('есть');
+            item.querySelector('.formula-item-popup-02').style.transform = 'rotate(0deg) translateY(-5px)';
+            item.querySelector('.formula-item__descr').style.opacity = '1';
+        } else if (item.querySelector('.formula-item-popup-03')) {
+            //console.log('есть');
+            item.querySelector('.formula-item-popup-03').style.transform = 'rotate(0deg) translateY(-5px)';
+            item.querySelector('.formula-item__descr').style.opacity = '1';
+        } else if (item.querySelector('.formula-item-popup-04')) {
+            //console.log('есть');
+            item.querySelector('.formula-item-popup-04').style.transform = 'rotate(0deg) translateY(-5px)';
+            item.querySelector('.formula-item__descr').style.opacity = '1';
+        } else if (item.querySelector('.formula-item-popup-05')) {
+            //console.log('есть');
+            item.querySelector('.formula-item-popup-05').style.transform = 'rotate(0deg) translateY(-5px)';
+            item.querySelector('.formula-item__descr').style.opacity = '1';
+            item.querySelector('.formula-item-popup-05 > div').style.marginTop = '0px';
+        } else if (item.querySelector('.formula-item-popup-06')) {
+            //console.log('есть');
+            item.querySelector('.formula-item-popup-06').style.transform = 'rotate(0deg) translateY(-5px)';
+            item.querySelector('.formula-item__descr').style.opacity = '1';
+        } else {
+            console.log('нету');
+        }
     };
     
     const observer = new IntersectionObserver((entries, observe) => {
@@ -93,26 +123,22 @@ const clue = () => {
             if (entry.isIntersecting) {
                 entry.target.style.zIndex = '1';
                 entry.target.classList.add('formula-item-active');
+                observer.unobserve(entry.target);
             } else if (!entry.isIntersecting) {
-                if (entry.target.classList.contains('formula-item-trans')) {
-                    entry.target.classList.remove('formula-item-trans');
-                    entry.target.querySelector('formula-item-descr').style.opacity = '1';
-                    //entry.target.style.transform = 'rotate(0deg) translateY(-5px)';
-                } else {
-                    addClueDown(entry.target);
-                }
-                
+                addClueDown(entry.target);
+                observer.unobserve(entry.target);
             }
         });
     }, { threshold: 0.9 });
 
     formulaItemIconInnerText.forEach(item => {
         item.addEventListener('mouseover', (e) => {
+            
             addClue(e.target);
             //observer.observe(e.target);
         });
         item.addEventListener('mouseout', (e) => {
-            //removeClue(e.target);
+            removeClue(e.target);
         });
     });
     
