2ef1abc3a86a702c9445ebeda4eae56047cfa33e
Sergey Tablet <martsnov@hotmail.com>
2022-01-22 22:58:59 +0300

    Начало табов

---
+++ src/modules/repairTabs.js
@@ -0,0 +1,8 @@
+const repairTabs = () => {
+    const tabs = document.querySelector('.repair-types-tab');
+    tabs.addEventListener('click', (e) => {
+        
+    });
+};
+
+export default repairTabs;
